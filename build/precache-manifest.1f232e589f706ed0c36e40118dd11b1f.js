a4a941126bf3b500ef325b2608bc2bdfa31cdb8b
martin <martinseguel53@hotmail.com>
2022-11-14 16:39:20 -0300

    Presentacion 2

---
+++ build/precache-manifest.1f232e589f706ed0c36e40118dd11b1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c9c0dd863dd3e0ad84e2da0a81d4cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddc787817c39f4a7801a",
+    "url": "/static/js/2.b4f37dff.chunk.js"
+  },
+  {
+    "revision": "555d3a88694cba337a75d4bf4f417a3a",
+    "url": "/static/js/2.b4f37dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6933fc2b2cf0e7febe7",
+    "url": "/static/js/main.0f463b07.chunk.js"
+  },
+  {
+    "revision": "ecc8794fefe937d5093b",
+    "url": "/static/js/runtime-main.8dccfd95.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
